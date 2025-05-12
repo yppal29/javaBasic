5e232c476130a03bce35de57888063f630e63073
yppal29 <yppal29@naver.com>
2025-05-12 17:44:29 +0900

    init commit

---
+++ javaStudy/HelloWorld.java
@@ -0,0 +1,15 @@
+package javaStudy;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("안녕, 난 이준원이야.");
+		
+		// 문자셋 설정 : 이 설정이 각각 틀리면, 한글 데이터나, 주석이 깨질수 있음.
+		// 때문에 반드시 문자셋 설정을 초기에 해둬야 나중에 후회 않음.
+		// 모든 문자셋은 UTF-8 로 설정함.
+		
+	}
+
+}
